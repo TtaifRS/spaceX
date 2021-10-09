cf9915056fd9e16feea6f39a389a2629d89c2d7f
Taif Rahaman <taif.rahaman73@gmail.com>
2021-10-09 18:03:56 +0600

    header added with searchbar

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,6 +5,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import { v4 as uuidv4 } from 'uuid';
 
 import CardComponet from '../components/CardComponent';
+import HeaderComponent from '../components/HeaderComponent';
+
 import { getDatas } from '../redux/features/datas/datasSlice';
 
 const HomePage = () => {
@@ -17,52 +19,53 @@ const HomePage = () => {
   }, []);
 
   return (
-    <Box display="flex" justifyContent="center" alignItems="center" sx={{ mx: '30px' }}>
-      <Grid container spacing={8}>
-        {datas && datas.map((data) => {
-          const {
-            mission_name,
-            launch_date_utc,
-            rocket,
-            launch_site,
-            launch_success,
-            links,
-            details,
-          } = data;
+    <>
+      <HeaderComponent />
+      <Box display="flex" justifyContent="center" alignItems="center" sx={{ mx: '30px', mt: '60px' }}>
+        <Grid container spacing={8}>
+          {datas && datas.map((data) => {
+            const {
+              mission_name,
+              launch_date_utc,
+              rocket,
+              launch_site,
+              launch_success,
+              links,
+              details,
+            } = data;
 
-          const {
-            mission_patch_small, article_link, wikipedia, video_link,
-          } = links;
+            const {
+              mission_patch_small, article_link, wikipedia, video_link,
+            } = links;
 
-          const {
-            rocket_name, rocket_type, second_stage,
-          } = rocket;
-          // eslint-disable-next-line no-console
-          console.log(datas.mission_name);
-          return (
-            <Grid item s={12} xs={12} md={3} key={uuidv4()}>
-              <CardComponet
-                missonName={mission_name}
-                date={launch_date_utc}
-                site={launch_site.site_name}
-                image={mission_patch_small}
-                article={article_link}
-                wikipedia={wikipedia}
-                video={video_link}
-                launchSuccess={launch_success}
-                info={details}
-                rocketName={rocket_name}
-                rocketType={rocket_type}
-                nationality={second_stage.payloads[0].nationality}
-                manufacturer={second_stage.payloads[0].manufacturer}
-                payloadType={second_stage.payloads[0].payload_type}
+            const {
+              rocket_name, rocket_type, second_stage,
+            } = rocket;
+            return (
+              <Grid item s={12} xs={12} md={3} key={uuidv4()}>
+                <CardComponet
+                  missonName={mission_name}
+                  date={launch_date_utc}
+                  site={launch_site.site_name}
+                  image={mission_patch_small}
+                  article={article_link}
+                  wikipedia={wikipedia}
+                  video={video_link}
+                  launchSuccess={launch_success}
+                  info={details}
+                  rocketName={rocket_name}
+                  rocketType={rocket_type}
+                  nationality={second_stage.payloads[0].nationality}
+                  manufacturer={second_stage.payloads[0].manufacturer}
+                  payloadType={second_stage.payloads[0].payload_type}
 
-              />
-            </Grid>
-          );
-        })}
-      </Grid>
-    </Box>
+                />
+              </Grid>
+            );
+          })}
+        </Grid>
+      </Box>
+    </>
   );
 };
 
